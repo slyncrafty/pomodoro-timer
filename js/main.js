7c43d2b4ea6eeff50b24ba4384bc3e944741f6d5
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-04-11 17:42:59 +0100

    Added to use localStorage to store previous Focus and Break Time.  HTML- added Break Time timer and default. CSS - count down when t <= 3 screen to lightly blink JS - Updates to modified timer setup to handle additional break timer including focus and break mode setups. Added beak time alert sound.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,63 @@
-const timer = document.getElementById('timer');
+const focusTime = document.getElementById('focus-time');
+const breakTime = document.getElementById('break-time');
+const timer = document.querySelector('.timer');
 const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
+const defaultBtn = document.getElementById('default-btn');
 const message = document.querySelector('.message');
-
-timer.addEventListener('change', setTimer);
 start.addEventListener('click', startTimer);
 stop.addEventListener('click', stopTimer);
 reset.addEventListener('click', resetTimer);
 
-let interval = null, endTime = null;
-let isRunning = false;
-
-window.addEventListener('DOMContentLoaded', () => {
-  timer.value = "25:00";
-  setTimer(); 
+defaultBtn.addEventListener('click', () => {
+  focusTime.value = '25:00';
+  breakTime.value = '05:00';
 });
 
+let interval = null, endTime = null;
+let isRunning = false, isBreak = false;
+let timeLeft = 0;
 
+// Event Listeners
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadSavedTimes();
+  resetTimer(); 
+});
+
+focusTime.addEventListener('change', () => setTimer(focusTime));
+breakTime.addEventListener('change', () => setTimer(breakTime));
+
+function loadSavedTimes() {
+  const storeFocus = localStorage.getItem('focusTime') || "25:00";
+  const storeBreak = localStorage.getItem('breakTime') || "05:00";
+  focusTime.value = storeFocus;
+  breakTime.value = storeBreak;
+}
+
+function saveTimes() {
+  localStorage.setItem('focusTime', focusTime.value);
+  localStorage.setItem('breakTime', breakTime.value);
+}
+
+// Format & Parse Input
 function parseTimeInput(input) {
   const regex = /^(\d{1,2}):([0-5]?\d)$/;
   const match = input.match(regex);
   if(!match){
     message.textContent = 'Invalid Time! Please Try Again.'
-    clearInterval(interval);
     return null;
   }
   [_, minutes, seconds] = match;
   minutes = parseInt(minutes, 10);
   seconds = parseInt(seconds, 10);
+  if (seconds > 59 || seconds > 59) {
+    message.textContent = 'Invalid Time! Please Try Again.';
+    return null;
+  } 
   // console.log(`${minutes}:${seconds}`)
+  message.textContent = ' ';
   return minutes * 60 + seconds;
 }
 
@@ -39,45 +67,79 @@ function formatTime(seconds){
   return `${min}:${sec}`;
 }
 
-function setTimer(){
-  const seconds = parseTimeInput(timer.value);
+function setTimer(inputElem){
+  const seconds = parseTimeInput(inputElem.value);
   if(!seconds) {
-    timer.value = formatTime(initialTime);
+    inputElem.value = formatTime(0);
     return;
   }
   timeLeft = seconds;
-  initialTime = seconds;
-  timer.value = formatTime(timeLeft);
-}
+  inputElem.value = formatTime(seconds);
+ }
 
 function updateTimer(){
   timer.value = formatTime(timeLeft);
-  message.textContent = 'Focus!'; 
 }
+
+function applyFocusMode() {
+  document.body.classList.remove('break-mode');
+  message.textContent = 'Focus! 🔥🔥🔥'; 
+}
+
+function applyBreakMode() {
+  document.body.classList.add('break-mode');
+  message.textContent = '👾👾👾 Break Time 👾👾👾'
+  breakTime.value = "BREAK" 
+}
+
 function startTimer() {
   if(isRunning) return;
-  setTimer();
+  isRunning = true;
+  isBreak = false;
+  saveTimes();
+  startMode(parseTimeInput(focusTime.value), "Focus");
+}
+
+function startMode(seconds, modeName) {
+  timeLeft = seconds;
   const now = Date.now();
   endTime = now + timeLeft * 1000;
-  isRunning = true;
-  timer.disabled = true;
-  message.textContent = 'Focus!'; 
 
+  if(modeName === "Focus") {
+    applyFocusMode();
+  } else {
+    applyBreakMode();
+  }
+  
+  focusTime.disabled = true;
+  breakTime.disabled = true;
+  clearInterval(interval);
   interval = setInterval(() => {
     const currTime = Date.now();
     timeLeft = Math.max(0, Math.round((endTime - currTime) / 1000));
     updateTimer();
+
     if(timeLeft <= 3) {
       timer.style.color = "red";
+      document.body.classList.add("blink-bg");
     }
+
     if(timeLeft === 0){
       clearInterval(interval);
       isRunning = false;
-      timer.style.color = "black";
-      timer.disabled = false;
-      // console.log("time's up");
-      alertSound.play();
-      message.textContent = 'Time is Up!'
+      timer.style.color = "";
+      document.body.classList.remove("blink-bg");
+
+      if(!isBreak) {
+        isBreak=true;
+        alertSound.play();
+        startMode(parseTimeInput(breakTime.value), "Break");
+      } else {
+        tripleBeep();
+        focusTime.disabled = false;
+        breakTime.disabled = false;
+        message.textContent = 'Session Complete 🎉🎉🎉'
+      }
     }
   }, 1000);
 }
@@ -86,17 +148,26 @@ function stopTimer() {
   clearInterval(interval);
   interval = null;
   isRunning = false;
-  timer.disabled = false;
+  focusTime.disabled = false;
+  breakTime.disabled = false;
+  message.textContent = 'Session Stopped'
+  document.body.classList.remove("blink-bg");
 }
 
 function resetTimer() {
   clearInterval(interval);
   interval = null;
   isRunning = false;
-  timeLeft = initialTime;
+  isBreak = false;
+  document.body.classList.remove('blink-bg');
+  applyFocusMode();
+  loadSavedTimes();
+  timeLeft = parseTimeInput(focusTime.value);
   updateTimer();
-  timer.style.color = "black";
-  timer.disabled = false;
+  timer.style.color = "";
+  focusTime.disabled = false;
+  breakTime.disabled = false;
+  
 }
 
 // Color Picker
@@ -105,7 +176,7 @@ const ftColorPicker = document.getElementById('font-color');
 
 bgColorPicker.addEventListener('input', () => {
   document.body.style.backgroundColor=bgColorPicker.value;
-  timer.style.backgroundColor=bgColorPicker.value;
+  document.querySelector('.timer').style.backgroundColor=bgColorPicker.value;
 });
 
 ftColorPicker.addEventListener('input', () => {
@@ -114,3 +185,25 @@ ftColorPicker.addEventListener('input', () => {
 
 // Alert Sound
 const alertSound = new Audio('sounds/Magic_Chime.mp3'); 
+
+function tripleBeep() {
+  const ctx = new AudioContext();
+  const factor = 0.5
+
+  for (let i = 0; i < 1; i++) {
+    const oscillator = ctx.createOscillator();
+    const gain = ctx.createGain();
+
+    oscillator.type = 'cosine';
+    oscillator.frequency.setValueAtTime(500, ctx.currentTime + i * factor); 
+
+    gain.gain.setValueAtTime(1, ctx.currentTime + i * factor);
+    gain.gain.setValueAtTime(0, ctx.currentTime + i * factor + 0.5);
+
+    oscillator.connect(gain);
+    gain.connect(ctx.destination);
+
+    oscillator.start(ctx.currentTime + i * factor);
+    oscillator.stop(ctx.currentTime + i * factor + 0.6);
+  }
+}
